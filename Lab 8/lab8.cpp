ce7bfdfa61cf2c0f792b899d36166b39404b695c
ChomaT <theochoma@gmail.com>
2016-11-02 13:22:24 -0500

    home commit 1

---
+++ Lab 8/lab8.cpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   lab8.cpp
+ * Author: theoc_000
+ *
+ * Created on November 2, 2016, 12:15 PM
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+class message{
+
+public: 
+    message();
+    message(string);
+    
+
+};
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
